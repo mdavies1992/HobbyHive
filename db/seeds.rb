42d855f65cb35e2028695da91f91f869e3307791
vincent de leonardis <vincent.deleo@gmail.com>
2024-06-19 17:27:18 +0900

    home page, basic seed and CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+UserHobby.delete_all
+Hobby.delete_all
+User.delete_all
+
+# Create hobbies
+hobby1 = Hobby.create!(name: 'Tennis', description: '...')
+hobby2 = Hobby.create!(name: 'Hiking', description: 'Exploring trails and nature.')
+
+# Create a user
+user = User.create!(
+  username: 'john_doe',
+  description: 'Avid tennis player and nature enthusiast',
+  latitude: 34.0522,
+  longitude: -118.2437,
+  location: 'Tokyo',
+  email: 'john@example.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+# Associate hobbies with the user
+UserHobby.create!(user: user, hobby: hobby1)
+UserHobby.create!(user: user, hobby: hobby2)
+
+puts "Database seeded successfully!"
